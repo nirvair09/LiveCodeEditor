f9b2a14b69185222e138157c3dcd8ec84db08c58
nirvair09 <chandradeep262003@gmail.com>
2025-12-27 21:19:01 +0530

    feat: add CodeMirror editor component for real-time code synchronization

--- client/src/components/editor.js
+++ client/src/components/editor.js
@@ -9,5 +9,52 @@ import ACTIONS from '../Actions';
 
 export const Editor = ({ socketRef, roomId, onCodeChange }) => {
     const editorRef = useRef(null);
+    useEffect(() => {
+        async function init() {
+            editorRef.current = Codemirror.fromTextArea(
+                document.getElementById('code'), {
+                mode: 'javascript',
+                theme: 'dracula',
+                autoCloseTags: true,
+                autoCloseBrackets: true,
+                lineNumbers: true,
+                lineWrapping: true,
+                matchBrackets: true,
+                foldGutter: true,
+                gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter'],
+            }
+            )
+        };
 
-}
+        editorRef.current.on('change', (instance, changes) => {
+            const { origin } = changes;
+
+            const code = instance.getValue();
+            onCodeChange(code);
+
+            if (origin !== 'setValue') {
+                socketRef.current.emit(ACTIONS.CODE_CHANGE, {
+                    roomId,
+                    code,
+                });
+            }
+        });
+        init();
+    }, []);
+
+    useEffect(() => {
+        if (socketRef.current) {
+            socketRef.current.on(ACTIONS.CODE_CHANGE, ({ code }) => {
+                if (code !== null) {
+                    editorRef.current.setValue(code);
+                }
+            });
+        }
+        return () => {
+            socketRef.current.off(ACTIONS.CODE_CHANGE);
+        }
+    }, [socketRef.current]);
+
+    return <textarea id='code' />
+
+};
