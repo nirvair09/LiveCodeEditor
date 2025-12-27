e4896f0f5f8b3751eff1ed17b1d4497eac2acbfc
nirvair09 <chandradeep262003@gmail.com>
2025-12-27 13:35:20 +0530

    feat: add client-side action types and a user display component

---
+++ client/src/components/user.js
@@ -0,0 +1,14 @@
+import react from "react";
+import Avatar from "react-avatar";
+
+const User = ({ username }) => {
+    return (
+        <div className="client">
+            <Avatar name={username} size="50" round="14px" />
+            <span className="userName">{username}</span>
+        </div>
+
+    );
+};
+
+export default User;
